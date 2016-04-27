6461c7490c92b0f75034f9f54a345cb227e24635
ajchambeaud <ajchambeaud@gmail.com>
2016-04-27 00:57:07 -0300

    implements properties and methods of the actionCreator function object (start, success and failure)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-const makeActionCreator = typePrefix => payload => ({
-  type: typePrefix.toUpperCase(),
-  payload
-});
+const makeActionCreator = typePrefix => {
+  const status = ['START', 'SUCCESS', 'FAILURE'];
+
+  typePrefix = typePrefix.toUpperCase();
+
+  const action = payload => ({
+    type: typePrefix,
+    payload
+  });
+
+  action.type = typePrefix;
+
+  status.forEach(type => {
+    action[type.toLowerCase()] = payload => ({
+      type: `${typePrefix}_${type}`,
+      payload
+    });
+    action[type] = `${typePrefix}_${type}`;
+  });
+
+  return action;
+};
 
 export default makeActionCreator;
--- test/index.js
+++ test/index.js
@@ -11,13 +11,76 @@ describe('makeActionCreator', () => {
     expect(actionCreator).to.be.a('function');
   });
 
-  it('should return an action object when action creator is called', () => {
-    const actionPrefixType = 'my_action';
-    const actionCreator = makeActionCreator(actionPrefixType);
+  describe('actionCreator', () => {
+    it('should return an action object wich include the payload and an uppercase type', () => {
+      const actionPrefixType = 'my_action';
+      const actionCreator = makeActionCreator(actionPrefixType);
 
-    expect(actionCreator('foo')).to.deep.equal({
-      type: 'MY_ACTION',
-      payload: 'foo'
+      expect(actionCreator('foo')).to.deep.equal({
+        type: 'MY_ACTION',
+        payload: 'foo'
+      });
+    });
+
+    it('should have property `type` equal to the uppercase action type', () => {
+      const actionPrefixType = 'my_action';
+      const actionCreator = makeActionCreator(actionPrefixType);
+
+      expect(actionCreator.type).to.be.equal('MY_ACTION');
+    });
+
+    it('should have method `start` that returns an action with the _START type sufix', () => {
+      const actionPrefixType = 'my_action';
+      const actionCreator = makeActionCreator(actionPrefixType);
+
+      expect(actionCreator.start).to.be.a('function');
+      expect(actionCreator.start('foo')).to.deep.equal({
+        type: 'MY_ACTION_START',
+        payload: 'foo'
+      });
+    });
+
+    it('should have property `START` equal to the uppercase action type with the _START sufix', () => {
+      const actionPrefixType = 'my_action';
+      const actionCreator = makeActionCreator(actionPrefixType);
+
+      expect(actionCreator.START).to.be.equal('MY_ACTION_START');
+    });
+
+    it('should have method `success` that returns an action with the _SUCCESS type sufix', () => {
+      const actionPrefixType = 'my_action';
+      const actionCreator = makeActionCreator(actionPrefixType);
+
+      expect(actionCreator.success).to.be.a('function');
+      expect(actionCreator.success('foo')).to.deep.equal({
+        type: 'MY_ACTION_SUCCESS',
+        payload: 'foo'
+      });
+    });
+
+    it('should have property `SUCCESS` equal to the uppercase action type with the _SUCCESS sufix', () => {
+      const actionPrefixType = 'my_action';
+      const actionCreator = makeActionCreator(actionPrefixType);
+
+      expect(actionCreator.SUCCESS).to.be.equal('MY_ACTION_SUCCESS');
+    });
+
+    it('should have method `failure` that returns an action with the _FAILURE type sufix', () => {
+      const actionPrefixType = 'my_action';
+      const actionCreator = makeActionCreator(actionPrefixType);
+
+      expect(actionCreator.failure).to.be.a('function');
+      expect(actionCreator.failure('foo')).to.deep.equal({
+        type: 'MY_ACTION_FAILURE',
+        payload: 'foo'
+      });
+    });
+
+    it('should have property `FAILURE` equal to the uppercase action type with the _FAILURE sufix', () => {
+      const actionPrefixType = 'my_action';
+      const actionCreator = makeActionCreator(actionPrefixType);
+
+      expect(actionCreator.FAILURE).to.be.equal('MY_ACTION_FAILURE');
     });
   });
 });
